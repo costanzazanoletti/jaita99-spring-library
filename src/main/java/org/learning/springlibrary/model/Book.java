2783d31c6b38fbc9bff280dd52a5e1d952798fc4
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2024-01-08 11:20:37 +0100

    Setup progetto, Entity Book, configurazione db

---
+++ src/main/java/org/learning/springlibrary/model/Book.java
@@ -0,0 +1,98 @@
+package org.learning.springlibrary.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+  // ATTRIBUTI
+  @Id // primary key
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+  private Integer id;
+  @Column(nullable = false)
+  private String title;
+  private String authors;
+  private String publisher;
+  @Column(nullable = false, length = 13)
+  private String isbn;
+  private Integer year;
+  @Lob
+  private String synopsis;
+  private LocalDateTime createdAt;
+
+  // COSTRUTTORI (se ne creo uno con parametri devo anche crearne uno vuoto)
+
+  // METODI (devo assolutamente mettere tutti i getter e setter)
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(String authors) {
+    this.authors = authors;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public String getSynopsis() {
+    return synopsis;
+  }
+
+  public void setSynopsis(String synopsis) {
+    this.synopsis = synopsis;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+}
