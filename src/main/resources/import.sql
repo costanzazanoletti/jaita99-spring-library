fb6858d9724b32e246edb433c287d7b8634ef5f2
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2024-01-17 13:05:17 +0100

    Aggiungo insert di BookType in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,6 +6,8 @@ INSERT INTO books (`year`, created_at, isbn, authors, publisher, title, synopsis
 INSERT INTO borrowings (book_id, expire_date, return_date, start_date, note) VALUES(1, '2024-01-20', null, '2023-12-20', '');
 INSERT INTO borrowings (book_id, expire_date, return_date, start_date, note) VALUES(1, '2023-11-10', '2023-11-11', '2023-10-10', 'Cover is stained');
 -- insert book_type
+INSERT INTO book_type (name) VALUES('hard cover');
+INSERT INTO book_type (name) VALUES('flexible cover');
 INSERT INTO book_type (name) VALUES('audiobook');
 INSERT INTO book_type (name) VALUES('e-book');
 INSERT INTO book_type (name) VALUES('ancient book');
