0039efb4b83a661d1fcd30c6d7f5b96ee26eb97a
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2024-01-17 11:22:45 +0100

    Update Borrowing

--- src/main/java/org/learning/springlibrary/controller/BorrowingController.java
+++ src/main/java/org/learning/springlibrary/controller/BorrowingController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -74,6 +75,40 @@ public class BorrowingController {
     Borrowing storedBorrowing = borrowingRepository.save(formBorrowing);
     // faccio una redirect alla pagina di dettaglio del libro
     return "redirect:/books/show/" + storedBorrowing.getBook().getId();
+  }
+
+  // metodo per restituire la pagina col form di modifica
+  @GetMapping("/edit/{id}") // http://localhost:8080/borrowings/edit/1
+  public String edit(@PathVariable Integer id, Model model) {
+    // recupero il Borrowing con quell'id da database
+    Optional<Borrowing> result = borrowingRepository.findById(id);
+    // se è presente precarico il form con il Borrowing
+    if (result.isPresent()) {
+      // lo passo come attributo al Model
+      model.addAttribute("borrowing", result.get());
+      // restituisco il template
+      return "borrowings/edit";
+    } else {
+      // altrimenti sollevo un'eccezione HTTP 404
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Borrowing with id " + id
+          + " not found");
+
+    }
+  }
+
+  // metodo per ricevere il submit del form di edit
+  @PostMapping("/edit/{id}") // http://localhost:8080/borrowings/edit/1
+  public String update(@PathVariable Integer id,
+      @Valid @ModelAttribute("borrowing") Borrowing formBorrowing, BindingResult bindingResult) {
+    // valido il Borrowing
+    if (bindingResult.hasErrors()) {
+      // se ci sono errori ricarico la pagina col form di edit
+      return "borrowings/edit";
+    }
+    // se non ci sono errori lo salvo su db
+    Borrowing updatedBorrowing = borrowingRepository.save(formBorrowing);
+    // redirect al dettaglio del libro prestato
+    return "redirect:/books/show/" + updatedBorrowing.getBook().getId();
   }
 
 }
